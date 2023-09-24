24e9df5fe230548ca11fcf160c9e74cd520a3e38
Hassan Adam <haskhr@HOTMAIL.COM>
2023-09-24 10:02:51 +0000

    #9 add random terrafrom provider

--- main.tf
+++ main.tf
@@ -1,2 +1,21 @@
-# this is first change
-# this is Guthub editing
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+}
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
