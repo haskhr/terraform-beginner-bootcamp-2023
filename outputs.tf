e8a8d36eefb9d918c162156d5d2fa70054dcabc8
Hassan Adam <haskhr@HOTMAIL.COM>
2023-10-01 21:34:16 +0000

    #25 refactor s3 bicket into the new terrahouse module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-
 output "bucket_name" {
-value = aws_s3_bucket.website_bucket.bucket
+    description = "bucket for our static web site hosting "
+    value = module.terrahouse_aws.bucket_name   
 }
