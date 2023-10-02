6b4c898cdb28db060d181f8ac95d1e4ad6250967
Hassan Adam <haskhr@HOTMAIL.COM>
2023-10-02 15:16:30 +0000

    #32 only upload file if content version changed

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -27,8 +27,11 @@ resource "aws_s3_object" "index_html" {
   key    = "index.html"
   source = var.index_html_filepath
   content_type = "text/html"
-
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [ etag ]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -37,8 +40,11 @@ resource "aws_s3_object" "error_html" {
   key    = "error.html"
   source = var.error_html_filepath
   content_type = "text/html"
-
   etag = filemd5(var.error_html_filepath)
+  # lifecycle {
+  #   #replace_triggered_by = [ var.content_version ]
+  #   ignore_changes = [ etag ]
+  # }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -62,4 +68,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
