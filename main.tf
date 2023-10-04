05daacae8bbf4375934964d2c816d84a5da42db5
haskhr <53363961+haskhr@users.noreply.github.com>
2023-10-04 11:59:30 +0000

    42 teraatowns provider (#45)
    * #42 wip building up skeleton for terratowns custom provider
    
    * #42 implement terratowns provider in terraform
    
    * #42 steup up skeleton for resource

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     terratowns = {
-        source = "local.providers/local/terratowns"
-        version = "1.0.0" 
-    
-    }
 
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
   }
-}
+  }
+
 
 provider "terratowns" {
 endponit = "http://localhost:4567"
@@ -15,6 +15,8 @@ user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
 token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
   
 }
+
+
 # module "terrahouse_aws" {
 # source = "./modules/terrahouse_aws"
 # user_uuid = var.user_uuid
