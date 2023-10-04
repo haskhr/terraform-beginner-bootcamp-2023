cf9cd3e2bacd954c0eff033fb2af2fc2689599d1
Hassan Adam <haskhr@HOTMAIL.COM>
2023-10-04 23:58:08 +0000

    #47 test our terra home on terratowns test town

--- variables.tf
+++ variables.tf
@@ -1,9 +1,19 @@
-variable "user_uuid" {
-  type= string
+variable "terratowns_endpoint" {
+ type = string
 }
-variable "bucket_name" {
-type=string
+
+variable "terratowns_access_token" {
+ type = string
 }
+
+variable "teacherseat_user_uuid" {
+ type = string
+}
+
+# variable "bucket_name" {
+#  type = string
+# }
+
 variable "index_html_filepath" {
   type = string
 }
